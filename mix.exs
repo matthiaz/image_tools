3114ddc7b0ddc523141aa645dc068325f16284be
Matthias Van Woensel <matthias.vanwoensel@platform.sh>
2024-08-09 15:48:07 +0200

    ensure that the native folder gets included

--- mix.exs
+++ mix.exs
@@ -32,7 +32,8 @@ defmodule ImageTools.MixProject do
     [
       name: "image_tools",
       licenses: ["Unlicense"],
-      links: %{"GitHub" => "https://github.com/matthiaz/image_tools"}
+      links: %{"GitHub" => "https://github.com/matthiaz/image_tools"},
+      files: ~w(mix.exs README.md lib native test .formatter.exs)
     ]
   end
 end
