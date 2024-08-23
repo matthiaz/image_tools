96804d5e5dce465d6bfb23f00710136fb843b49a
Matthias Van Woensel <matthias.vanwoensel@platform.sh>
2024-08-23 13:45:58 +0200

    ensure sourcing is done in rc file

--- lib/mix/tasks/precompile.ex
+++ lib/mix/tasks/precompile.ex
@@ -4,7 +4,10 @@ defmodule Mix.Tasks.Precompile do
   @shortdoc "Checks and installs Rust and Cargo before compilation"
 
   def run(_args) do
-    unless system_has_rust?() do
+    Mix.shell().info("Checking if rust and cargo are already installed...")
+    if system_has_rust?() do
+      Mix.shell().info("Yep, all good.")
+    else
       Mix.shell().info("Rust and Cargo not found. Installing...")
       install_rust()
     end
@@ -20,8 +23,10 @@ defmodule Mix.Tasks.Precompile do
         Mix.raise("Please install Rust and Cargo manually on Windows: https://rustup.rs")
 
       _ ->
-        Mix.shell().cmd("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y", into: IO.stream(:stdio, :line))
-        Mix.shell().cmd("source $HOME/.cargo/env", into: IO.stream(:stdio, :line))
+        Mix.shell().info("Downloading for Linux...")
+        Mix.shell().cmd("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal", into: IO.stream(:stdio, :line))
+        Mix.shell().cmd(". $HOME/.cargo/env", into: IO.stream(:stdio, :line))
+        Mix.shell().cmd("touch $HOME/.bashrc && echo 'source $HOME/.cargo/env' >> $HOME/.bashrc", into: IO.stream(:stdio, :line))
     end
   end
 end
